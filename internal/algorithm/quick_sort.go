3220c9b5f4cb33b75140bfff1ed3c2a89968522c
chenxing <468046048@QQ.com>
2025-02-27 16:32:06 +0800

    init：初始化

---
+++ internal/algorithm/quick_sort.go
@@ -0,0 +1,19 @@
+package algorithm
+
+func QuickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	pivot := arr[0]
+	var left, right []int
+	for _, v := range arr[1:] {
+		if v < pivot {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+	left = QuickSort(left)
+	right = QuickSort(right)
+	return append(append(left, pivot), right...)
+}
